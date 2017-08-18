c65da027788e0e5924413aea6ec9b9d25ae6a891
Digoritos <rmc.morelli@gmail.com>
2017-08-17 22:25:43 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1503018707,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/mach-sheet0.png",
+		"images/bg.png",
+		"images/tubos-sheet0.png",
+		"images/tubos-sheet1.png",
+		"images/tubos-sheet2.png",
+		"images/pc-sheet0.png",
+		"images/pc-sheet1.png",
+		"images/engineer-sheet0.png",
+		"images/engineer-sheet1.png",
+		"images/venti-sheet0.png",
+		"images/venti-sheet1.png",
+		"images/venti-sheet2.png",
+		"images/laser-sheet0.png",
+		"images/laser-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
